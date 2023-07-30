919821b060bbef6ca09185545ab14323318441eb
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-07-30 12:16:10 -0300

    Aplicando testes de unidade para Produto e Categoria

---
+++ tests/ProductCatalog.Domain.Tests/ProductUnitTest.cs
@@ -0,0 +1,75 @@
+﻿using FluentAssertions;
+using ProductCatalog.Domain.Entities;
+using ProductCatalog.Domain.Validation;
+
+namespace ProductCatalog.Domain.Tests
+{
+    public class ProductUnitTest
+    {
+        [Fact(DisplayName = "Create Product With Valid State")]
+        public void CreateProduct_WithValidParameters_ResultObjectValidState() 
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "product image");
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create Product With Negative Id")]
+        public void CreateProduct_NegativeIdValue_DomainExceptionInvalidId() 
+        {
+            Action action = () => new Product(-1, "Product Name", "Product Description", 9.99m, 99, "product image");
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid Id value");
+        }
+
+        [Fact(DisplayName = "Create Product With Short Name")]
+        public void CreateProduct_ShortNameValue_DomainExceptionShortName() 
+        {
+            Action action = () => new Product(1, "Pr", "Product Description", 9.99m, 99, "product image");
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid name, too short, minimum 3 characters");
+        }
+
+        [Fact(DisplayName = "Create Product With Long Image Name")]
+        public void CreateProduct_LongImageName_DomainExceptionLongImageName() 
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m,
+                99, "product image toooooooooooooooooooooooooooooooooooooooooooo loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooogggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg");
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid image name, too long, maximum 250 characters");
+        }
+
+        [Fact(DisplayName = "Create Product With Null Image Name")]
+        public void CreateProduct_WithNullImageName_NoDomainException() 
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create Product With Null Referenced Image Name")]
+        public void CreateProduct_WithNullImageName_NoNullReferenceException() 
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
+            action.Should().NotThrow<NullReferenceException>();
+        }
+
+        [Fact(DisplayName = "Create Product With Empty Image Name")]
+        public void CreateProduct_WithEmptyImageName_NoDomainException() 
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "");
+            action.Should().NotThrow<DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create Product With Invalid Price")]
+        public void CreateProduct_InvalidPriceValue_DomainException() 
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", -9.99m, 99, "");
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid price value");
+        }
+
+        [Theory(DisplayName = "Create Product With Negative Stock Value")]
+        [InlineData(-1)]
+        [InlineData(-2)]
+        public void CreateProduct_InvalidStockValue_ExceptionDomainNegativeValue(int value) 
+        {
+            Action action = () => new Product(1, "Pro", "Product Description", 9.99m, value, "product image");
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid stock value");
+        }
+    }
+}
