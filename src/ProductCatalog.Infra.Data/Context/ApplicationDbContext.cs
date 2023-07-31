b8c02c0e2d49c142b086c56e5328548a399ba481
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-07-31 16:02:13 -0300

    Implementando a camada de infraestrutura

---
+++ src/ProductCatalog.Infra.Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Domain.Entities;
+
+namespace ProductCatalog.Infra.Data.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
