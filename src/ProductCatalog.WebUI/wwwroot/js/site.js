46e4f8fca01f81232292c8cce94347e85c2e6beb
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-08-11 14:16:42 -0300

    Refatorando a implementação do spinner

--- src/ProductCatalog.WebUI/wwwroot/js/site.js
+++ src/ProductCatalog.WebUI/wwwroot/js/site.js
@@ -1,6 +1,31 @@
-﻿let filterButton = document.getElementById("filterButton");
+﻿function showSpinner() {
+    let spinnerOverlay = document.getElementById('spinnerOverlay');
+
+    if (spinnerOverlay) {
+        spinnerOverlay.style.display = 'flex';
+    }
+}
+
+function hideSpinner() {
+    let spinnerOverlay = document.getElementById('spinnerOverlay');
+
+    if (spinnerOverlay) {
+        spinnerOverlay.style.display = 'none';
+    }
+}
+
+window.addEventListener('load', function () {
+    showSpinner();
+    setTimeout(function () {
+        hideSpinner();
+    }, 1000);
+});
+
+let filterButton = document.getElementById("filterButton");
 
 filterButton.addEventListener("click", function () {
+    showSpinner();
+
     let filterInput = document.getElementById("filterInput");
     let filterValue = encodeURIComponent(filterInput.value);
 
@@ -8,14 +33,8 @@ filterButton.addEventListener("click", function () {
     let url = `${indexUrl}?pageNumber=${pageNumber}&filter=${filterValue}`;
 
     window.location.href = url;
-});
-
-window.addEventListener('load', function () {
-    let spinnerOverlay = document.getElementById('spinnerOverlay');
-    spinnerOverlay.style.display = 'block';
 
     setTimeout(function () {
-        spinnerOverlay.style.display = 'none';
-    }, 500);
+        hideSpinner();
+    }, 1000);
 });
-
