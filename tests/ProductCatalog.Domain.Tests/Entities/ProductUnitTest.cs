552d91accc8750061769f1ee1af2a80ae58381d0
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-08-11 21:07:03 -0300

    Organizando estrutura dos testes das entidades

--- tests/ProductCatalog.Domain.Tests/Entities/ProductUnitTest.cs
+++ tests/ProductCatalog.Domain.Tests/Entities/ProductUnitTest.cs
@@ -2,33 +2,33 @@
 using ProductCatalog.Domain.Entities;
 using ProductCatalog.Domain.Validation;
 
-namespace ProductCatalog.Domain.Tests
+namespace ProductCatalog.Domain.Tests.Entities
 {
     public class ProductUnitTest
     {
         [Fact(DisplayName = "Create Product With Valid State")]
-        public void CreateProduct_WithValidParameters_ResultObjectValidState() 
+        public void CreateProduct_WithValidParameters_ResultObjectValidState()
         {
             Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "product image");
             action.Should().NotThrow<DomainExceptionValidation>();
         }
 
         [Fact(DisplayName = "Create Product With Negative Id")]
-        public void CreateProduct_NegativeIdValue_DomainExceptionInvalidId() 
+        public void CreateProduct_NegativeIdValue_DomainExceptionInvalidId()
         {
             Action action = () => new Product(-1, "Product Name", "Product Description", 9.99m, 99, "product image");
             action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid Id value");
         }
 
         [Fact(DisplayName = "Create Product With Short Name")]
-        public void CreateProduct_ShortNameValue_DomainExceptionShortName() 
+        public void CreateProduct_ShortNameValue_DomainExceptionShortName()
         {
             Action action = () => new Product(1, "Pr", "Product Description", 9.99m, 99, "product image");
             action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid name, too short, minimum 3 characters");
         }
 
         [Fact(DisplayName = "Create Product With Long Image Name")]
-        public void CreateProduct_LongImageName_DomainExceptionLongImageName() 
+        public void CreateProduct_LongImageName_DomainExceptionLongImageName()
         {
             Action action = () => new Product(1, "Product Name", "Product Description", 9.99m,
                 99, "product image toooooooooooooooooooooooooooooooooooooooooooo loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooogggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg");
@@ -36,28 +36,28 @@ namespace ProductCatalog.Domain.Tests
         }
 
         [Fact(DisplayName = "Create Product With Null Image Name")]
-        public void CreateProduct_WithNullImageName_NoDomainException() 
+        public void CreateProduct_WithNullImageName_NoDomainException()
         {
             Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
             action.Should().NotThrow<DomainExceptionValidation>();
         }
 
         [Fact(DisplayName = "Create Product With Null Referenced Image Name")]
-        public void CreateProduct_WithNullImageName_NoNullReferenceException() 
+        public void CreateProduct_WithNullImageName_NoNullReferenceException()
         {
             Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
             action.Should().NotThrow<NullReferenceException>();
         }
 
         [Fact(DisplayName = "Create Product With Empty Image Name")]
-        public void CreateProduct_WithEmptyImageName_NoDomainException() 
+        public void CreateProduct_WithEmptyImageName_NoDomainException()
         {
             Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "");
             action.Should().NotThrow<DomainExceptionValidation>();
         }
 
         [Fact(DisplayName = "Create Product With Invalid Price")]
-        public void CreateProduct_InvalidPriceValue_DomainException() 
+        public void CreateProduct_InvalidPriceValue_DomainException()
         {
             Action action = () => new Product(1, "Product Name", "Product Description", -9.99m, 99, "");
             action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid price value");
@@ -66,7 +66,7 @@ namespace ProductCatalog.Domain.Tests
         [Theory(DisplayName = "Create Product With Negative Stock Value")]
         [InlineData(-1)]
         [InlineData(-2)]
-        public void CreateProduct_InvalidStockValue_ExceptionDomainNegativeValue(int value) 
+        public void CreateProduct_InvalidStockValue_ExceptionDomainNegativeValue(int value)
         {
             Action action = () => new Product(1, "Pro", "Product Description", 9.99m, value, "product image");
             action.Should().Throw<DomainExceptionValidation>().WithMessage("Invalid stock value");
