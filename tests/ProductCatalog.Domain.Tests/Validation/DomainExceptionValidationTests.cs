9184a5c08c095a7e2bcb80abb09b165d613be837
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-08-15 15:23:17 -0300

    Criando testes de unidade para tratamento de exceção

---
+++ tests/ProductCatalog.Domain.Tests/Validation/DomainExceptionValidationTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using ProductCatalog.Domain.Validation;
+
+namespace ProductCatalog.Domain.Tests.Validation
+{
+    public class DomainExceptionValidationTests
+    {
+        [Fact(DisplayName = "When - No Errors No Exception Thrown")]
+        public void DomainExceptionValidation_When_NoErrorsNoExceptionThrown()
+        {
+
+            Action action = () => DomainExceptionValidation.When(false, "An error occurred.");
+
+            action.Should().NotThrow();
+        }
+
+        [Fact(DisplayName = "When - Errors Domain Exception Thrown")]
+        public void DomainExceptionValidation_When_ErrorsDomainExceptionThrown()
+        {
+
+            Action action = () => DomainExceptionValidation.When(true, "An error occurred.");
+
+            action.Should().Throw<DomainExceptionValidation>().WithMessage("An error occurred.");
+        }
+    }
+}
