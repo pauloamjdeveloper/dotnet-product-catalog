91f625d7c1205c46d71bdc4884fc2af854f9c032
pauloamjdeveloper <developer.pamj4973@gmail.com>
2023-09-02 11:28:56 -0300

    Criando testes para as interfaces de serviços  em Application

---
+++ tests/ProductCatalog.Application.Tests/Interfaces/CategoryServiceTest.cs
@@ -0,0 +1,216 @@
+﻿using Moq;
+using ProductCatalog.Application.DTOs;
+using ProductCatalog.Application.Interfaces;
+using ProductCatalog.Application.Utilities;
+
+namespace ProductCatalog.Application.Tests.Interfaces
+{
+    public class CategoryServiceTest
+    {
+        List<CategoryDTO> listCategories = new List<CategoryDTO>
+        {
+            new CategoryDTO { Id = 1, Name = "Material Escolar" },
+            new CategoryDTO { Id = 2, Name = "Eletrônicos" },
+            new CategoryDTO { Id = 3, Name = "Acessórios" },
+        };
+
+        [Fact(DisplayName = "GetCategoriesPaginated - Return Paginated All List Categories")]
+        public async Task CategoryService_GetCategoriesPaginated_ShouldReturnPaginatedAllListCategories()
+        {
+            var mockCategoryService = new Mock<ICategoryService>();
+            var pageNumber = 1;
+            var pageSize = 10;
+            var filter = "Categories";
+
+            var paginatedList = new PaginatedList<CategoryDTO>(listCategories, listCategories.Count, pageNumber, pageSize);
+
+            mockCategoryService.Setup(service => service.GetCategoriesPaginated(pageNumber, pageSize, filter))
+                .ReturnsAsync(paginatedList);
+
+            var categoryService = mockCategoryService.Object;
+
+            var result = await categoryService.GetCategoriesPaginated(pageNumber, pageSize, filter);
+
+            Assert.NotNull(result);
+            Assert.Equal(pageNumber, result.PageIndex);
+        }
+
+        [Fact(DisplayName = "GetCategoriesPaginated - Return Invalid Paginated List Categories")]
+        public async Task CategoryService_GetCategoriesPaginated_ShouldReturnEmptyPaginatedAllListCategories()
+        {
+            var mockCategoryService = new Mock<ICategoryService>();
+            var pageNumber = -1;
+            var pageSize = 0;
+            var filter = "Categories";
+
+            mockCategoryService.Setup(service => service.GetCategoriesPaginated(pageNumber, pageSize, filter))
+                .ReturnsAsync(() => throw new ArgumentException("Invalid parameters"));
+
+            var categoryService = mockCategoryService.Object;
+
+            var exception = await Assert.ThrowsAsync<ArgumentException>(
+                async () => await categoryService.GetCategoriesPaginated(pageNumber, pageSize, filter)
+            );
+
+            Assert.Equal("Invalid parameters", exception.Message);
+        }
+
+        [Fact(DisplayName = "GetCategories - Return All Categories")]
+        public async Task CategoryService_GetCategories_ShouldReturnAllCategories()
+        {
+            var mockCategoryService = new Mock<ICategoryService>();
+            
+            mockCategoryService.Setup(service => service.GetCategories()).ReturnsAsync(listCategories);
+
+            var categoryService = mockCategoryService.Object;
+
+            var result = await categoryService.GetCategories();
+
+    
+            Assert.NotNull(result);
+            Assert.Equal(listCategories.Count, result.Count());
+        }
+
+        [Fact(DisplayName = "GetCategories - Return Empty List When No Categories Exist")]
+        public async Task CategoryService_GetCategories_ShouldReturnEmptyListWhenNoCategoriesExist()
+        {
+            var mockCategoryService = new Mock<ICategoryService>();
+            var emptyCategories = new List<CategoryDTO>();
+
+            mockCategoryService.Setup(service => service.GetCategories()).ReturnsAsync(emptyCategories);
+
+            var categoryService = mockCategoryService.Object;
+
+            var result = await categoryService.GetCategories();
+
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+
+        [Fact(DisplayName = "GetById - Returns CategoryDTO When Id exists")]
+        public async Task CategoryService_GetById_ShouldReturnCategoryDTOWhenIdExists()
+        {
+            var mockCategoryService = new Mock<ICategoryService>();
+            var categoryId = 1;
+            var categoryDto = new CategoryDTO { Id = categoryId, Name = "Material Escolar" };
+
+            mockCategoryService.Setup(service => service.GetById(categoryId)).ReturnsAsync(categoryDto);
+
+            var categoryService = mockCategoryService.Object;
+
+            var result = await categoryService.GetById(categoryId);
+
+            Assert.NotNull(result);
+            Assert.Equal(categoryId, result.Id);
+        }
+
+        [Fact(DisplayName = "GetById - Returns null when id does not exist")]
+        public async Task CategoryService_GetById_ShouldReturnNullWhenCategoryDTOIdDoesNotExist()
+        {
+            var mockCategoryService = new Mock<ICategoryService>();
+            var invalidCategoryId = 9999;
+
+            mockCategoryService.Setup(service => service.GetById(invalidCategoryId)).ReturnsAsync((CategoryDTO)null);
+
+            var categoryService = mockCategoryService.Object;
+
+            var result = await categoryService.GetById(invalidCategoryId);
+
+            Assert.Null(result);
+        }
+
+        [Fact(DisplayName = "Add - Return The Added CategoryDTO")]
+        public async Task CategoryService_Add_ShouldInsertCategoryAndReturnAddedCategoryDTO()
+        {
+            var mockCategoryService = new Mock<ICategoryService>();
+            var categoryDto = new CategoryDTO { Name = "Livros" };
+
+            mockCategoryService.Setup(service => service.Add(categoryDto)).Verifiable();
+
+            var categoryService = mockCategoryService.Object;
+
+            await categoryService.Add(categoryDto);
+
+            mockCategoryService.Verify();
+        }
+
+        [Fact(DisplayName = "Add - Returns Null When Add Fails")]
+        public async Task CategoryService_Add_ShouldReturnNullWhenAddedCategoryDTOFails()
+        {
+            var mockCategoryService = new Mock<ICategoryService>();
+            var invalidCategoryDto = new CategoryDTO { Name = null };
+
+            mockCategoryService.Setup(service => service.Add(invalidCategoryDto))
+                .ThrowsAsync(new InvalidOperationException("Livros"));
+
+            var categoryService = mockCategoryService.Object;
+
+            Func<Task> act = async () => await categoryService.Add(invalidCategoryDto);
+
+            await Assert.ThrowsAsync<InvalidOperationException>(act);
+            mockCategoryService.Verify(service => service.Add(invalidCategoryDto), Times.Once);
+        }
+
+        [Fact(DisplayName = "Update - Return Updated CategoryDTO")]
+        public async Task CategoryService_Update_ShouldUpdateReturnUpdatedCategoryDTO()
+        {
+            var mockCategoryService = new Mock<ICategoryService>();
+            var updatedCategoryDto = new CategoryDTO { Id = 1, Name = "Materiais Escolares" };
+
+            mockCategoryService.Setup(service => service.Update(updatedCategoryDto)).Verifiable();
+
+            var categoryService = mockCategoryService.Object;
+
+            await categoryService.Update(updatedCategoryDto);
+
+            mockCategoryService.Verify();
+        }
+
+        [Fact(DisplayName = "Update - Returns Null When CategoryDTO Not Found")]
+        public async Task CategoryService_Update_ShouldReturnNullWhenCategoryDTONotFound()
+        {
+            var mockCategoryService = new Mock<ICategoryService>();
+            var invalidCategoryDto = new CategoryDTO { Id = 1, Name = null };
+
+            mockCategoryService.Setup(service => service.Update(invalidCategoryDto))
+                .ThrowsAsync(new InvalidOperationException("Invalid category update"));
+
+            var categoryService = mockCategoryService.Object;
+
+            Func<Task> act = async () => await categoryService.Update(invalidCategoryDto);
+
+            await Assert.ThrowsAsync<InvalidOperationException>(act);
+        }
+
+        [Fact(DisplayName = "Remove - Returns The Existing CategoryDTO Delete")]
+        public async Task CategoryService_Remove_ShouldRemoveCategoryAndReturnRemovedCategoryDTO()
+        {
+            var mockCategoryService = new Mock<ICategoryService>();
+            var categoryId = 1;
+
+            mockCategoryService.Setup(service => service.Remove(categoryId)).Verifiable();
+
+            var categoryService = mockCategoryService.Object;
+
+            await categoryService.Remove(categoryId);
+
+            mockCategoryService.Verify();
+        }
+
+        [Fact(DisplayName = "Remove - Returns Null When CategoryDTO Not Found")]
+        public async Task CategoryService_Remove_ShouldReturnNullWhenCategoryDTONotFound()
+        {
+            var mockCategoryService = new Mock<ICategoryService>();
+            var invalidCategoryId = 9999;
+
+            mockCategoryService.Setup(service => service.Remove(invalidCategoryId))
+                .ThrowsAsync(new InvalidOperationException("Invalid category removal"));
+
+            var categoryService = mockCategoryService.Object;
+
+            Func<Task> act = async () => await categoryService.Remove(invalidCategoryId);
+
+            await Assert.ThrowsAsync<InvalidOperationException>(act);
+        }
+    }
+}
---
+++ tests/ProductCatalog.Application.Tests/Interfaces/ProductServiceTest.cs
@@ -0,0 +1,283 @@
+﻿using Moq;
+using ProductCatalog.Application.DTOs;
+using ProductCatalog.Application.Interfaces;
+using ProductCatalog.Application.Utilities;
+
+namespace ProductCatalog.Application.Tests.Interfaces
+{
+    public class ProductServiceTest
+    {
+        List<ProductDTO> listProducts = new List<ProductDTO>
+        {
+            new ProductDTO
+            {
+                Id = 1,
+                Name = "Caderno Espiral",
+                Description = "Caderno espiral com 100 fôlhas",
+                Price = 7.99m,
+                Stock = 50,
+                Image = "caderno-1.png",
+                CategoryId = 1
+            },
+            new ProductDTO
+            {
+                Id = 2,
+                Name = "Calculadora escolar",
+                Description = "Calculadora simples",
+                Price = 15.39m,
+                Stock = 20,
+                Image = "calculadora.png",
+                CategoryId = 2
+            },
+            new ProductDTO
+            {
+                Id = 3,
+                Name = "Chaveiro Chip",
+                Description = "Chaveiro personalizado em formato chip",
+                Price = 30.90M,
+                Stock = 30,
+                Image = "chaveiro.png",
+                CategoryId = 3
+            },
+        };
+
+
+        [Fact(DisplayName = "GetProductsPaginated - Return Paginated All List Products")]
+        public async Task ProductService_GetProductsPaginated_ShouldReturnPaginatedAllListProducts()
+        {
+           var mockProductService = new Mock<IProductService>();
+            var pageNumber = 1;
+            var pageSize = 10;
+            var filter = "Products";
+
+            var paginatedList = new PaginatedList<ProductDTO>(listProducts, listProducts.Count, pageNumber, pageSize);
+
+            mockProductService.Setup(service => service.GetProductsPaginated(pageNumber, pageSize, filter)).ReturnsAsync(paginatedList);
+
+            var productService = mockProductService.Object;
+
+            var result = await productService.GetProductsPaginated(pageNumber, pageSize, filter);
+
+            Assert.NotNull(result);
+            Assert.Equal(pageNumber, result.PageIndex);
+        }
+
+        [Fact(DisplayName = "GetProductsPaginated - Return Invalid Paginated List Products")]
+        public async Task ProductService_GetProductsPaginated_ShouldReturnEmptyPaginatedAllListProducts()
+        {
+            var mockProductService = new Mock<IProductService>();
+            var pageNumber = -1;
+            var pageSize = 0;
+            var filter = "Products";
+
+            mockProductService.Setup(service => service.GetProductsPaginated(pageNumber, pageSize, filter))
+                .ReturnsAsync(new PaginatedList<ProductDTO>(new List<ProductDTO>(), 0, pageNumber, pageSize));
+
+            var productService = mockProductService.Object;
+
+            var result = await productService.GetProductsPaginated(pageNumber, pageSize, filter);
+
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+
+        [Fact(DisplayName = "GetProducts - Return All Products")]
+        public async Task ProductService_GetProducts_ShouldReturnAllProducts()
+        {
+            var mockProductService = new Mock<IProductService>();
+
+            mockProductService.Setup(service => service.GetProducts()).ReturnsAsync(listProducts);
+
+            var productService = mockProductService.Object;
+
+            var result = await productService.GetProducts();
+
+            Assert.NotNull(result);
+            Assert.Equal(listProducts.Count, result.Count());
+        }
+
+        [Fact(DisplayName = "GetProducts - Return Empty List When No Products Exist")]
+        public async Task ProductService_GetProducts_ShouldReturnEmptyListWhenNoProductsExist()
+        {
+            var mockProductService = new Mock<IProductService>();
+            var emptyProducts = new List<ProductDTO>();
+
+            mockProductService.Setup(service => service.GetProducts()).ReturnsAsync(emptyProducts);
+
+            var productService = mockProductService.Object;
+
+            var result = await productService.GetProducts();
+
+            Assert.NotNull(result);
+            Assert.Empty(result);
+        }
+
+        [Fact(DisplayName = "GetById - Returns ProductDTO When Id exists")]
+        public async Task ProductService_GetById_ShouldReturnProductDTOWhenIdExists()
+        {
+            var mockProductService = new Mock<IProductService>();
+            var productId = 1;
+            var productDto = new ProductDTO
+            {
+                Id = 1,
+                Name = "Caderno Espiral",
+                Description = "Caderno espiral com 100 fôlhas",
+                Price = 7.99m,
+                Stock = 50,
+                Image = "caderno-1.png",
+                CategoryId = 1
+            };
+
+            mockProductService.Setup(service => service.GetById(productId)).ReturnsAsync(productDto);
+
+            var productService = mockProductService.Object;
+
+            var result = await productService.GetById(productId);
+
+            Assert.NotNull(result);
+            Assert.Equal(productId, result.Id);
+        }
+
+        [Fact(DisplayName = "GetById - Returns null when id does not exist")]
+        public async Task ProductService_GetById_ShouldReturnNullWhenProductDTOIdDoesNotExist()
+        {
+            var mockProductService = new Mock<IProductService>();
+            var invalidProductId = 9999;
+
+            mockProductService.Setup(service => service.GetById(invalidProductId)).ReturnsAsync((ProductDTO)null);
+
+            var productService = mockProductService.Object;
+
+            var result = await productService.GetById(invalidProductId);
+
+            Assert.Null(result);
+        }
+
+        [Fact(DisplayName = "Add - Return The Added ProductDTO")]
+        public async Task ProductService_Add_ShouldInsertProductAndReturnAddedProductDTO()
+        {
+            var mockProductService = new Mock<IProductService>();
+            var productDto = new ProductDTO
+            {
+                Name = "Agenda",
+                Description = "Agenda escolar",
+                Price = 10.00M,
+                Stock = 15,
+                CategoryId = 1
+            };
+
+            mockProductService.Setup(service => service.Add(productDto)).Verifiable();
+
+            var productService = mockProductService.Object;
+
+            await productService.Add(productDto);
+
+            mockProductService.Verify();
+        }
+
+        [Fact(DisplayName = "Add - Returns Null When Add Fails")]
+        public async Task ProductService_Add_ShouldReturnNullWhenAddedProductDTOFails()
+        {
+            var mockProductService = new Mock<IProductService>();
+            var invalidProductDto = new ProductDTO
+            {
+                Description = "Descrição invalida",
+                Price = 0M, 
+                Stock = 50,
+                CategoryId = 2
+            };
+
+            mockProductService.Setup(service => service.Add(invalidProductDto)).ThrowsAsync(new ArgumentException("Invalid product"));
+
+            var productService = mockProductService.Object;
+
+            Func<Task> act = async () => await productService.Add(invalidProductDto);
+
+            await Assert.ThrowsAsync<ArgumentException>(act); 
+            mockProductService.Verify(service => service.Add(invalidProductDto), Times.Once);
+        }
+
+        [Fact(DisplayName = "Update - Return Updated CategoryDTO")]
+        public async Task ProductService_Update_ShouldUpdateReturnUpdatedProductDTO()
+        {
+            var mockProductService = new Mock<IProductService>();
+            var updatedProductDto = new ProductDTO
+            {
+                Id = 1,
+                Name = "Caderno Espiral",
+                Description = "Caderno espiral com 100 fôlhas",
+                Price = 8.99m,
+                Stock = 70,
+                Image = "caderno-1.png",
+                CategoryId = 1
+            };
+
+            mockProductService.Setup(service => service.Update(updatedProductDto)).Verifiable();
+
+            var productService = mockProductService.Object;
+
+            await productService.Update(updatedProductDto);
+
+            mockProductService.Verify();
+        }
+
+        [Fact(DisplayName = "Update - Returns Null When ProductDTO Not Found")]
+        public async Task ProductService_Update_ShouldReturnNullWhenProductDTONotFound()
+        {
+            var mockProductService = new Mock<IProductService>();
+            var invalidProductDto = new ProductDTO
+            {
+                Id = 1,
+                Name = null,
+                Description = "Caderno espiral com 100 fôlhas",
+                Price = 8.99m,
+                Stock = 70,
+                Image = "caderno-1.png",
+                CategoryId = 1
+            };
+            
+            mockProductService.Setup(service => service.Update(invalidProductDto))
+                .ThrowsAsync(new ArgumentException("Invalid product update"));
+
+            var productService = mockProductService.Object;
+
+            Func<Task> act = async () => await productService.Update(invalidProductDto);
+
+            await Assert.ThrowsAsync<ArgumentException>(act);
+            mockProductService.Verify(service => service.Update(invalidProductDto), Times.Once);
+        }
+
+
+        [Fact(DisplayName = "Remove - Returns The Existing ProductDTO Delete")]
+        public async Task ProductService_Remove_ShouldRemoveProductAndReturnRemovedProductDTO()
+        {
+            var mockProductService = new Mock<IProductService>();
+            var productId = 1;
+
+            mockProductService.Setup(service => service.Remove(productId)).Verifiable();
+
+            var productService = mockProductService.Object;
+
+            await productService.Remove(productId);
+
+            mockProductService.Verify();
+        }
+
+        [Fact(DisplayName = "Remove - Returns Null When ProductDTO Not Found")]
+        public async Task ProductService_Remove_ShouldReturnNullWhenProductDTONotFound()
+        {
+            var mockProductService = new Mock<IProductService>();
+            var invalidProductId = 9999;
+
+            mockProductService.Setup(service => service.Remove(invalidProductId))
+                .ThrowsAsync(new InvalidOperationException("Invalid product removal"));
+
+            var productService = mockProductService.Object;
+
+            Func<Task> act = async () => await productService.Remove(invalidProductId);
+
+            await Assert.ThrowsAsync<InvalidOperationException>(act);
+        }
+
+    }
+}
